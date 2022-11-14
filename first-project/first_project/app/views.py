6a7b21e43958063e3d36f067a0d80121bfaf0373
Bushuev Aleksandr DevOps-23 <sasha047@mail.ru>
2022-11-14 10:32:34 +0700

    edut urls adn views

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -8,10 +8,10 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse(time),
+        'Показать содержимое рабочей директории': reverse(workdir)
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -21,15 +21,21 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
-    raise NotImplemented
+    list_work = []
+    path = '.'
+    rez = os.listdir(path)
+    for n in rez:
+        list_work.append(f'{n},')
+    return HttpResponse(list_work)
+    # raise NotImplemented
